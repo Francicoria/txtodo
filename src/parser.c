bf5605c6b298a383ba7e3a5cfba17f4718e3923b
Franci <franci@VoidBox.com>
2022-10-01 22:19:16 +0200

    Parsing kinda works

--- src/parser.c
+++ src/parser.c
@@ -1,31 +1,63 @@
 #include <stdio.h>
+#include <string.h>
 
-enum Status {
-	ACTIVE,
-	FINISHED
-};
-
-typedef struct {
-	char * task;
-	int status;
-	char priority_letter;
-	char * contexts;
-	char * tags;
-} Task;
-
-void se
-
-void fileToTasks(FILE * file, Task * tasks) {
-	int line = 0;
-	int str_index = 0;
-	for (int c; (c = getc(file)) != EOF;) {
-		if (c == '\n') {
-			++line;
-			str_index = 0;
-		} else tasks[line].task[str_index++] = c;
+Task fileLineToTask(FILE * file) {
+	Task task;
+	task.task[0] = '\0';
+	int strIndex = 0;
+	for (int c; (c = getc(file)) != '\n';) {
+		if (c == EOF) break;
+		task.task[strIndex++] = c;
 	}
+	task.task[strIndex] = '\0';
+	return task;
 }
 
-Task * parseTasks(Task * tasks) {
-	return tasks;
+enum Check {
+	FINISH,
+	PRIORITY,
+	CONTEXTS_TAGS
+};
+
+Task parseTask(Task task) {
+	int check = FINISH;
+	char * token;
+
+	task.contexts[0] = '\0';
+	task.tags[0] = '\0';
+
+	token = strtok(task.task, " ");
+	while (token != NULL) {
+		switch (check) {
+			case FINISH:
+				if (token[0] == 'x' &&
+				    token[1] == '\0') {
+					task.status = 420;
+				} else {
+					task.status = ACTIVE;
+					check = PRIORITY;
+				}
+				break;
+			case PRIORITY:
+				token = strtok(NULL, " ");
+				if (token[0] == '(' &&
+				    token[2] == ')' &&
+				    token[3] == '\0') {
+					task.priorityLetter = token[1];
+				} else {
+					task.priorityLetter = '#';
+					check = CONTEXTS_TAGS;
+				}
+				break;
+			case CONTEXTS_TAGS:
+				if (token[0] == '@' &&
+				    token[1] != '\0') strcat(task.contexts, "pog ");
+				if (token[0] == '+' &&
+				    token[1] != '\0') strcat(task.tags, "non ");
+
+				token = strtok(NULL, " ");
+				break;
+		}
+	}
+	return task;
 }
--- src/start.c
+++ src/start.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
+// if the user exceeds any of these caps, i would be impressed.
+#define MAX_TASKS          70
+#define MAX_CONTEXTS_SIZE  40
+#define MAX_TAGS_SIZE      40
+#define MAX_TASK_SIZE      (MAX_CONTEXTS_SIZE + MAX_TAGS_SIZE) + 50
+
+
 enum Status {
-	ACTIVE,
-	FINISHED
+	ACTIVE = 69,
+	FINISHED = 420
 };
 
 typedef struct {
-	char * task;
+	char task[MAX_TASK_SIZE];
 	int status;
-	char priority_letter;
-	char * contexts;
-	char * tags;
+	char priorityLetter;
+	char contexts[MAX_CONTEXTS_SIZE];
+	char tags[MAX_TAGS_SIZE];
 } Task;
 
 #include "parser.c"
@@ -21,16 +28,34 @@ int main(int argc, char ** argv) {
 
 	switch (argc) {
 		case 1:
+			// TODO
 			printf("No argument passed, assuming you want to create a new todo.txt file.\n");
 			break;
 		case 2:
 			filename = argv[1];
-			if ((fp = fopen(filename, "rw")) == NULL) {
+			if ((fp = fopen(filename, "r+")) == NULL) {
 				fprintf(stderr, "%s does not exist or is not readable/writable.\n", filename);
 				return 1;
 			}
+			Task tasks[MAX_TASKS];
+			for (int i = 0; i < MAX_TASKS; ++i) {
+				tasks[i] = fileLineToTask(fp);
+				// If the first character of a line is end of line (\0)
+				// i assume that the rest of the file is empty.
+				if (tasks[i].task[0] == '\0') break;
+				//printf("%s\n", tasks[i].task);
+
+				tasks[i] = parseTask(tasks[i]);
+				printf("Task n°%d\nIs finished: %d\nPriority letter: %c\nContexts: %s\nTags: %s\n",
+				       i + 1,
+				       tasks[i].status,// == FINISHED ? "yes" : "no",
+				       // TODO fixare la merda
+				       tasks[i].priorityLetter,
+				       tasks[i].contexts,
+				       tasks[i].tags
+				       );
+			}
 			//printf("\x1b[3mFilename: \x1b[32m%s\x1b[m\n", argv[1]);
-			fileToTasks(fp, tasks);
 			//parseFile(fp);
 			fclose(fp);
 			break;
@@ -40,4 +65,5 @@ int main(int argc, char ** argv) {
 	}
 
 	putc('\n', stdout);
+	return 0;
 }
